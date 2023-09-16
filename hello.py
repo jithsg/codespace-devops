a50440b7779a438fe5a96a995d93dc4a1ebea298
jithsg <135303624+jithsg@users.noreply.github.com>
2023-09-16 13:33:04 +0000

    Tested

--- hello.py
+++ hello.py
@@ -1,8 +1,10 @@
-#A command line program using fire module to get two numbers from the user and print the sum of the two numbers
+# A command line program using fire module to get two numbers from the user and print the sum of the two numbers
 import fire
+
 
 def add(x, y):
     return x + y
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     fire.Fire(add)
---
+++ test_hello.py
@@ -0,0 +1,9 @@
+# test file to check the hello.py file
+import hello
+
+
+def test_hello():
+    assert hello.add(1, 2) == 3
+    assert hello.add(1, 3) == 4
+    assert hello.add(1, 4) == 5
+    assert hello.add(1, 5) == 6
