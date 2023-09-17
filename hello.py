d42ae31170afe6d3f4b33afcbc61f5c21922f4ea
jithsg <135303624+jithsg@users.noreply.github.com>
2023-09-17 15:11:52 +0000

    added comments

--- hello.py
+++ hello.py
@@ -1,12 +1,10 @@
 # A command line program using fire module to get two numbers from the user and print the sum of the two numbers
 import fire
 
-# function to add two numbers
-
+# python function to add two numbers
 
 def add(x, y):
     return x + y
-
 
 if __name__ == "__main__":
     fire.Fire(add)
